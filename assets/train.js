05a83bbdb0e0412117a497d40666fe1ff19c0ba7
Jared <jared.harris1@g.austincc.edu>
2018-09-18 16:47:41 -0500

    for grade

--- assets/train.js
+++ assets/train.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
     /*FireBase
     ==============================================================*/
-    // Initialize Firebase
-  // Initialize Firebase
+      // Initialize Firebase
   var config = {
     apiKey: "AIzaSyB6BUGf-MEBlV7Bz5-nKWZgPNGHfW8DU2E",
     authDomain: "train-app-2.firebaseapp.com",
@@ -12,104 +11,104 @@ $(document).ready(function(){
     messagingSenderId: "54665152759"
   };
   firebase.initializeApp(config);
-    
-      //Reference database
-      database = firebase.database();
-    
+
+    //Reference database
+    database = firebase.database();
+
     /*Global letiables
     ==============================================================*/
     let trainName = '';
     let dest = '';
-    let firstTrainTime = ''; 
+    let firstTrainTime = '';
     let freq = '';
-    
+
     //Conversion letiable
     let firstTimeConverted = '';
     let diffTime = '';
     let tRemainder;
     let tMinutesTillTrain;
-    let nextTrain; 
-    
+    let nextTrain;
+
     //Data reference
     let trainNameData = '';
     let destData = '';
     let arrivalData = '';
     let freqData = '';
-    let minutesAwayData = ''; 
-     
+    let minutesAwayData = '';
+
+//testing
+    let clickCounter = 0;
+
+        $("#test-button").on('click', function () {
+            clickCounter++;
+            database.ref().set({
+                clickcount: clickCounter
+
+            });
+            console.log(clickCounter);
+        });
+
     /*Functions
     ==============================================================*/
-        //When Submit button is clicked.....
-        $('#submit').on('click',function(event){
-            console.log("click");
-            event.preventDefault();
-            //Get input info
-            trainName = $('#trainName').val().trim();
-            dest = $('#dest').val().trim();
-            firstTrainTime = $('#firstTrainTime').val().trim();
-            freq = $('#freq').val().trim();
-    
-            //Removed input info 
-            $('#trainName').val('');
-            $('#dest').val('');
-            $('#firstTrainTime').val('');
-            $('#freq').val('');
-    
-            
-            //Conversion
-                //Convert to HH:MM
-                firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-                //Converts the firsTimeCover object into string
-                
-                 // Current Time
-                let currentTime = moment();
-                diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-                
-                // Time apart (remainder)
-                tRemainder = diffTime % freq;
-    
-                // Minute Until Train
-                tMinutesTillTrain = freq - tRemainder;
-                
-                // Next Train
-                nextTrain = moment().add(tMinutesTillTrain, "minutes");
-                nextTrainFormat = moment(nextTrain).format('hh:mm');
-            
+    //When Submit button is clicked.....
+    $('#submit-button').on('click', function (event) {
+        event.preventDefault();
+        //Get input info
+        trainName = $('#trainName').val().trim();
+        dest = $('#dest').val().trim();
+        firstTrainTime = $('#firstTrainTime').val().trim();
+        freq = $('#freq').val().trim();
 
-            console.log("about to write to db");
+        //Removed input info 
+        $('#trainName').val('');
+        $('#dest').val('');
+        $('#firstTrainTime').val('');
+        $('#freq').val('');
 
-            database.ref('/trainSchedule').push({
-                trainName: trainName,
-                destination: dest,
-                arrival: nextTrainFormat,
-                minutesAway: tMinutesTillTrain,
-                frequency: freq 
-            }); 	
-        }); 
-            
-            
-            database.ref('/trainSchedule').on('child_added',function(snap){
-                        //Testing
-                        trainNameData = snap.val().trainName;
-                        destData = snap.val().destination;
-                        arrivalData = snap.val().arrival;
-                        freqData = snap.val().frequency;
-                        minutesAwayData = snap.val().minutesAway;
-    
-                        //Data array
-                        let dataArray = [trainNameData, destData, freqData, arrivalData, minutesAwayData];
-                        let newTr = $('<tr>');
-                        for(let i = 0; i< dataArray.length; i++){
-                            let newTd = $('<td>');
-                            newTd.text(dataArray[i]);
-                            newTd.appendTo(newTr);
-                        }	
-                        $('.table').append(newTr);
-            });
-            
+        //Conversion
+        //Convert to HH:MM
+        firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+        //Converts the firsTimeCover object into string
 
-    });// End of line
+        // Current Time
+        let currentTime = moment();
+        diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-    // function googleTranslateElementInit() {
-    //     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-    //   }
+        // Time apart (remainder)
+        tRemainder = diffTime % freq;
+
+        // Minute Until Train
+        tMinutesTillTrain = freq - tRemainder;
+
+        // Next Train
+        nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        nextTrainFormat = moment(nextTrain).format('hh:mm');
+
+        database.ref('/trainSchedule').push({
+            trainName: trainName,
+            destination: dest,
+            arrival: nextTrainFormat,
+            minutesAway: tMinutesTillTrain,
+            frequency: freq
+        });
+    });
+
+    database.ref('/trainSchedule').on('child_added', function (snap) {
+        //Testing
+        trainNameData = snap.val().trainName;
+        destData = snap.val().destination;
+        arrivalData = snap.val().arrival;
+        freqData = snap.val().frequency;
+        minutesAwayData = snap.val().minutesAway;
+
+        //Data array
+        let dataArray = [trainNameData, destData, freqData, arrivalData, minutesAwayData];
+        let newTr = $('<tr>');
+        for (let i = 0; i < dataArray.length; i++) {
+            let newTd = $('<td>');
+            newTd.text(dataArray[i]);
+            newTd.appendTo(newTr);
+        }
+        $('.table').append(newTr);
+    });
+});// End of line
